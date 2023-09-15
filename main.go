ae8c4f7cf5d18894f03b41fa3a28e7c6b9e15fc1
Scott Semtner <sjsemtner@gmail.com>
2023-09-14 22:17:38 -0700

    basic caching

--- main.go
+++ main.go
@@ -7,6 +7,7 @@ import (
 	"log"
 	"net/http"
 	"os"
+	"strings"
 	"time"
 
 	"github.com/chromedp/chromedp"
@@ -54,21 +55,77 @@ func main() {
 		"user": *tokenFlag,
 	}))
 
+	// real ip from nginx
+	r.Use(middleware.RealIP)
+
+	// this doubles as the warm up call
+	cachedCookies := ""
+
+	// start a warm up call
+	go func() {
+		cookies := GetCookies(ctx, term, username, password)
+
+		// update the cached cookies
+		cachedCookies = strings.Clone(cookies)
+	}()
+
 	r.Get("/", func(w http.ResponseWriter, r *http.Request) {
 		start := time.Now()
 		log.Printf("Request received from %s\n", r.RemoteAddr)
-		cookies := GetCookies(ctx, term, username, password)
+
+		// make a copy so they can't change mid request (this is not good still)
+		cookies := strings.Clone(cachedCookies)
+
+		// test if the cookies are still valid by trying to get the schedule list
+		req, err := http.NewRequest("GET", "https://act.ucsd.edu/webreg2/svc/wradapter/secure/sched-get-schednames?termcode=FA23", nil)
+		if err != nil {
+			log.Printf("Request failed from %s in %s\n", r.RemoteAddr, time.Since(start))
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
+
+		req.Header.Set("Cookie", cookies)
+
+		client := &http.Client{
+			CheckRedirect: func(req *http.Request, via []*http.Request) error {
+				return http.ErrUseLastResponse
+			},
+		}
+
+		resp, err := client.Do(req)
+		if err != nil {
+			log.Printf("Request failed from %s in %s\n", r.RemoteAddr, time.Since(start))
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
+
+		if resp.StatusCode != http.StatusOK {
+			log.Println("cookies are invalid, running auth flow...")
+			cookies = GetCookies(ctx, term, username, password)
+
+			// update the cached cookies
+			cachedCookies = strings.Clone(cookies)
+		}
+
+		body := make([]byte, resp.ContentLength)
+		resp.Body.Read(body)
+		log.Printf("%v", string(body))
+
 		log.Printf("Request completed from %s in %s\n", r.RemoteAddr, time.Since(start))
 
 		w.Write([]byte(cookies))
 	})
 
-	r.Get("/test", func(w http.ResponseWriter, r *http.Request) {
-		w.Write([]byte("TEST"))
-	})
+	r.Get("/force", func(w http.ResponseWriter, r *http.Request) {
+		start := time.Now()
+		log.Printf("Request received (FORCE) from %s\n", r.RemoteAddr)
+		cookies := GetCookies(ctx, term, username, password)
+		log.Printf("Request completed (FORCE) from %s in %s\n", r.RemoteAddr, time.Since(start))
 
-	// warm up call
-	GetCookies(ctx, term, username, password)
+		w.Write([]byte(cookies))
+
+		cachedCookies = strings.Clone(cookies)
+	})
 
 	http.ListenAndServe(fmt.Sprintf(":%d", *portFlag), r)
 }
