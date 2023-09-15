69f16f92f18e218f430991068aee18ad75a1ef7d
Scott Semtner <sjsemtner@gmail.com>
2023-09-15 04:15:10 +0000

    reduce to simple cookie server

--- go.mod
+++ go.mod
@@ -1,9 +1,8 @@
-module webreg
+module webregautologin
 
-go 1.19
+go 1.18
 
 require (
-	github.com/bwmarrin/discordgo v0.27.1
 	github.com/chromedp/cdproto v0.0.0-20230909221021-38a8736298fe
 	github.com/chromedp/chromedp v0.9.2
 	github.com/go-chi/chi v1.5.5
@@ -14,9 +13,7 @@ require (
 	github.com/gobwas/httphead v0.1.0 // indirect
 	github.com/gobwas/pool v0.2.1 // indirect
 	github.com/gobwas/ws v1.2.1 // indirect
-	github.com/gorilla/websocket v1.4.2 // indirect
 	github.com/josharian/intern v1.0.0 // indirect
 	github.com/mailru/easyjson v0.7.7 // indirect
-	golang.org/x/crypto v0.0.0-20210421170649-83a5a9bb288b // indirect
 	golang.org/x/sys v0.6.0 // indirect
 )
--- main.go
+++ main.go
@@ -1,13 +1,63 @@
 package main
 
 import (
+	"context"
 	"flag"
-	"webreg/discord"
+	"fmt"
+	"log"
+	"net/http"
+	"os"
+	"time"
+
+	"github.com/chromedp/chromedp"
+
+	"github.com/go-chi/chi"
 )
 
 func main() {
-	token := flag.String("token", "", "discord bot token")
+	termFlag := flag.String("term", "", "term code")
+	portFlag := flag.Int("port", 3000, "port to listen on")
+	usernameFlag := flag.String("username", "", "UCSD username")
+	passwordFlag := flag.String("password", "", "UCSD password")
+
 	flag.Parse()
 
-	discord.Run(*token)
+	term, err := ParseTerm(*termFlag)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	username := *usernameFlag
+	password := *passwordFlag
+
+	if username == "" {
+		username = os.Getenv("WEBREG_USERNAME")
+	}
+
+	if password == "" {
+		password = os.Getenv("WEBREG_PASSWORD")
+	}
+
+	if username == "" || password == "" {
+		log.Fatal("please provide a username and password via flags or the WEBREG_USERNAME and WEBREG_PASSWORD environment variables")
+	}
+
+	ctx, cancel := chromedp.NewContext(context.Background())
+	defer cancel()
+
+	r := chi.NewRouter()
+
+	r.Get("/", func(w http.ResponseWriter, r *http.Request) {
+		start := time.Now()
+		log.Printf("Request received from %s\n", r.RemoteAddr)
+		cookies := GetCookies(ctx, term, username, password)
+		log.Printf("Request completed from %s in %s\n", r.RemoteAddr, time.Since(start))
+
+		w.Write([]byte(cookies))
+	})
+
+	// warm up call
+	GetCookies(ctx, term, username, password)
+
+	http.ListenAndServe(fmt.Sprintf(":%d", *portFlag), r)
 }
